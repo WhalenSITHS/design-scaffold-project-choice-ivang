66e38945a4c96e0fdd15d9e234d6f490fed5b5db
Yuno1106 <aiyuno1106@gmail.com>
2022-02-11 10:38:25 -0500

    a

---
+++ a/javascript/domselectors.js
@@ -0,0 +1,2 @@
+let domselector = {};
+export { domselector };
---
+++ a/javascript/main.js
@@ -0,0 +1,4 @@
+import { domselector } from "./domselectors";
+import { array, cart } from "./array";
+import "../styles/style.css";
+import "../styles/index.css";
