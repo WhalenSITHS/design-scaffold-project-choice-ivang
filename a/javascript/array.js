a4c7866ee625c14b22925b7ce6cb85fd20006e7e
Yuno1106 <aiyuno1106@gmail.com>
2022-02-16 10:33:41 -0500

    Revert "a"
    This reverts commit 744e6fac53a89417eea8f12b78baef0276e0e116.

---
+++ a/javascript/array.js
@@ -0,0 +1,3 @@
+let array = [];
+
+export { array };
---
+++ a/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./javascript/main.js",
+    "./javascript/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
