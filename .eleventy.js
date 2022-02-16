fb2d467efe763aa82636a83cbcd9cab6fd5abb06
Yuno1106 <aiyuno1106@gmail.com>
2022-02-16 10:34:22 -0500

    Revert "Revert "a""
    This reverts commit a4c7866ee625c14b22925b7ce6cb85fd20006e7e.

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughcopy("src/style");
-  eleventyConfig.addPassthroughcopy("src/md");
-  eleventyConfig.addPassthroughcopy("src/media");
-  return {
-    dir: { input: "src", output: "dist" },
-  };
-};
