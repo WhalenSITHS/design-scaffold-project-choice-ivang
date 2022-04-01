c07413fddec9c9b0323ef622b56010af797841d2
Yuno1106 <aiyuno1106@gmail.com>
2022-04-01 10:20:43 -0400

    A

--- eleventy-sample/.eleventy.js
+++ eleventy-sample/.eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/md");
   eleventyConfig.addPassthroughCopy("src/media");
+  eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
